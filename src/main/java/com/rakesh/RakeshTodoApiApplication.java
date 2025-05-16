bc863f95e2fc32bdc595c031f516733feb35effc
rakeshshekhawat <rsshekhawat04@gmail.com>
2025-05-16 17:02:34 +0530

    advance changes

--- src/main/java/com/rakesh/RakeshTodoApiApplication.java
+++ src/main/java/com/rakesh/RakeshTodoApiApplication.java
@@ -2,6 +2,12 @@ package com.rakesh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class RakeshTodoApiApplication {
@@ -10,4 +16,9 @@ public class RakeshTodoApiApplication {
 		SpringApplication.run(RakeshTodoApiApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI springToDoOpenApi () {
+		return new OpenAPI().info(new Info().title("Rakesh Todo Api").description("Demo app").version("v0.0.1").license(new License().
+				name("Apache 2.0.1").url("www.springboot.org"))).externalDocs(new ExternalDocumentation().description("ToDo Documentation").url("https://github.com/rakeshshekhawat/rakesh-todo-api"));
+	}
 }
